86ef5cb1385961c0a218895a2086241ca4a8ac2d
Shoukri <saljarjawi@gmail.com>
2020-11-17 11:52:06 +0200

    test

--- client/src/components/students/profile.js
+++ client/src/components/students/profile.js
@@ -5,7 +5,7 @@ export default function DashBord() {
 
   return (
     <>
-      {/* {currentUser.fullName ? (
+      {currentUser.fullName ? (
         <section id="main-content">
           <section class="wrapper ">
             <div class="row">
@@ -38,8 +38,8 @@ export default function DashBord() {
             </div>
           </section>
         </section>
-      ) : null} */}
-      <div>Hellow</div>
+      ) : null}
+      
     </>
   );
 }
